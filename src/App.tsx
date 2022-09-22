c709a128a69aad4d632569d5f45a171f1318de2a
Marcelo Alves <alves-marcelo97@hotmail.com>
2022-09-22 04:07:40 -0300

    base app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {PlusCircle, Clipboard, Trash, Check} from 'phosphor-react';
+import * as Checkbox from '@radix-ui/react-checkbox';
+
+import styles from './App.module.scss';
+import './global.scss';
+
+import clipboardIcon from './assets/clipboardIcon.svg'
+import logoImg from './assets/logoImg.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <header className={styles.header}>
+        <img src={logoImg} alt="A rocket launching" />
+        to<span>do</span>
+      </header>
+      <div className={styles.wrapper}>
+        <div className={styles.inputContainer}>
+          <input placeholder='Adicione uma nova tarefa' />
+          <button>Criar <PlusCircle weight='bold' /></button>
+        </div>
+
+        <div className={styles.todoListContainer}>
+          <header>
+            <strong className={styles.todosCount}>Tarefas criadas <span className={styles.badge}>0</span></strong>
+            <strong className={styles.todosCompleted}>Concluídas <span className={styles.badge}>0</span></strong>
+          </header>
+          {/* <div className={styles.emptyTodos}>
+            <img src={clipboardIcon} alt="A written clipboard icon" />
+            <span>Você ainda não tem tarefas cadastradas</span>
+            <span>Crie tarefas e organize seus itens a fazer</span>
+          </div> */}
+          <div className={styles.todoListContent}>
+            <div className={styles.todo}>
+              <Checkbox.Root className={styles.checkboxRoot}>
+                <Checkbox.Indicator className={styles.checkboxIndicator}>
+                  <Check />
+                </Checkbox.Indicator>
+              </Checkbox.Root>
+              <span>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
+              <Trash />
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
