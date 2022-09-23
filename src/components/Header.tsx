24645b23099ec2f279724078086d13fab94ee654
Marcelo Alves <alves-marcelo97@hotmail.com>
2022-09-23 18:40:47 -0300

    add h1 descriptive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,10 @@ export function Header() {
   return (
     <header className={styles.header}>
       <img src={logoImg} alt="A rocket launching" />
-      to<span>do</span>
+      <h1>
+        to<span className={styles.purpleText}>do</span>
+        <span className={styles.description}>Sua lista de tarefas</span>
+      </h1>
     </header>
   )
 }
