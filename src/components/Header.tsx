0156dbfff9825796043e251106c1c78906d18cf8
Marcelo Alves <alves-marcelo97@hotmail.com>
2022-09-23 19:14:34 -0300

    changed header title style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,14 @@ import logoImg from '../assets/logoImg.svg';
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={logoImg} alt="A rocket launching" />
       <h1>
-        to<span className={styles.purpleText}>do</span>
+        <span>
+          <img src={logoImg} alt="A rocket launching" />
+          to<span className={styles.purpleText}>do</span>
+        </span>
         <span className={styles.description}>Sua lista de tarefas</span>
       </h1>
+      
     </header>
   )
 }
